34c7a8bc7be3cc3d2eab53746d6f048ce70d7418
Ym <1098548238@qq.com>
2022-05-05 11:35:41 +0800

    调整项目结构

--- Assets/Scripts/DesignPattern/Command.cs
+++ Assets/Scripts/DesignPattern/Command.cs
@@ -5,14 +5,6 @@ public interface ICommand
     void Execute(GameActor gameActor);
 }
 
-public class JumpCommand : ICommand
-{
-    public void Execute(GameActor gameActor)
-    {
-        gameActor.Jump();
-    }
-}
-
 public class AttackCommand : ICommand
 {
     public void Execute(GameActor gameActor)
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -16,6 +16,8 @@ public class Player : GameActor
     private float inputX;
     [SerializeField] private float moveSpeed;
     [SerializeField] private float jumpForce;
+    [SerializeField] private float fallMultiplier = 3.0f;
+    [SerializeField] private float lowJumpMultiplier = 1.5f;
     [SerializeField] private float sprintSpeed;
     [SerializeField] private LayerMask groundLayer;
 
@@ -39,8 +41,9 @@ public class Player : GameActor
     [SerializeField] private bool chargeDone;//蓄力完成
     [SerializeField] private bool charging;//是否在蓄力中
 
-    [Header("技能相关")] 
-    private bool canSprint, sprintDone;
+    [Header("特殊攻击")] 
+    private bool canSprint, lastSprintDone;
+    private bool isAttack, beAttacked;
 
     public ICommand playerCommand;
 
@@ -49,24 +52,33 @@ public class Player : GameActor
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         collider2D = GetComponent<Collider2D>();
-        //默认武器为匕首
-        weapon = GetComponent<Dagger>();
+        weapon = GetComponent<Dagger>();//默认武器为匕首
         //添加受击函数
         GetComponent<Attacked>().OnGetHit += OnGetHurt;
         //初始化人物属性
         hp = maxHp;
-        sprintDone = true;
+        //默认上一次冲刺结束
+        lastSprintDone = true;
     }
     
+    /// <summary>
+    /// 角色被销毁时移除受击函数
+    /// </summary>
+    private void OnDestroy()
+    {
+        GetComponent<Attacked>().OnGetHit -= OnGetHurt;
+    }
+
     private void Update()
     {
         GetInput();
         UpdateAnimator();
+        Jump();
         playerCommand?.Execute(this);
         if (canSprint)
         {
-            Debug.Log("冲刺中");
             rb.velocity = new Vector2(transform.localScale.x * 10 * sprintSpeed, rb.velocity.y);
+            Debug.Log("冲刺中");
         }
     }
     private void FixedUpdate()
@@ -84,18 +96,19 @@ public class Player : GameActor
         
     }
     /// <summary>
-    /// 更新Animator参数
+    /// 实时更新Animator参数
     /// </summary>
     private void UpdateAnimator()
     {
         animator.SetFloat(speed, Math.Abs(inputX));//输入绝对值作为speed参数
-        animator.SetBool(isIdle, inputX==0);//输入为0时处于idle状态
-        animator.SetBool(isWalk, inputX>0);//输入不为0时处于walk状态
+        animator.SetBool(isIdle, inputX == 0);//输入为0时处于idle状态
+        animator.SetBool(isWalk, inputX > 0);//输入不为0时处于walk状态
         animator.SetBool("canSprint", canSprint);
-        animator.SetBool("sprintDone", sprintDone);
+        animator.SetBool("sprintDone", lastSprintDone);
 
         if (animator.GetBool(jumping))
         {
+            //跳跃状态中y轴速度衰减为0时开始下落
             if(rb.velocity.y < 0)
             {
                 animator.SetBool(jumping, false);
@@ -104,9 +117,10 @@ public class Player : GameActor
         }
         if (animator.GetBool(falling))
         {
+            //接触到地面时停止下坠
             if(collider2D.IsTouchingLayers(groundLayer))
             {
-                animator.SetBool(falling, false);//接触到地面时停止下坠
+                animator.SetBool(falling, false);
             }
         }
     }
@@ -117,35 +131,50 @@ public class Player : GameActor
     }
 
     /// <summary>
-    /// 移动函数
+    /// 移动函数，由本脚本获得的输入控制
     /// </summary>
     private void Move()
     {
-        if (inputX != 0 && !canSprint)
+        if (inputX != 0 && !canSprint && !isAttack && !beAttacked)
         {
             //具体的移动
             rb.velocity = new Vector2(inputX * moveSpeed * Time.fixedDeltaTime, rb.velocity.y);
             //人物转向
+            Debug.Log(inputX);
             transform.localScale = new Vector3(inputX * 0.1f, 0.1f, 0.1f);
         }
     }
 
     /// <summary>
-    /// 跳跃函数
+    /// 跳跃，由命令模式调用
     /// </summary>
-    public override void Jump()
+    private void Jump()
     {
         //处于地面才能跳跃
-        if (collider2D.IsTouchingLayers(groundLayer))
+        if (collider2D.IsTouchingLayers(groundLayer) && Input.GetButtonDown("Jump"))
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
             animator.SetBool(jumping, true);
+            Debug.Log("Player进行跳跃");
+        }
+        //通过调整下落时的加速度调整跳跃手感
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (rb.velocity.y > 0 && !Input.GetButton("Jump"))
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
         }
     }
-    
+
+    /// <summary>
+    /// 普通攻击，由命令模式调用
+    /// </summary>
     public override void Attack()
     {
         animator.Play("knife_attack1");
+        isAttack = true;//攻击结束将其置为false
         Debug.Log("Player进行普通攻击");
     }
 
@@ -171,6 +200,7 @@ public class Player : GameActor
     private void OnGetHurt(Vector2 position,Vector2 force,int damage)
     {
         hp -= damage;
+        beAttacked = true;//受击动画播放完将其置为false
         Debug.Log("Player被攻击");
     }
 
@@ -204,15 +234,16 @@ public class Player : GameActor
         
     }
 
-    #region 冲刺相关
+    #region 冲刺
     public override void Sprint()
     {
-        if (sprintDone)
+        if (lastSprintDone)
         {
             //sprintDone为true即上一次冲刺结束时才能进行下一次冲刺
             animator.Play("knife_sprint");
+            isAttack = true;//别忘了在冲刺结束设置为false
             Debug.Log("Player进入冲刺准备状态");
-            sprintDone = false;
+            lastSprintDone = false;
         }
     }
     /// <summary>
@@ -230,12 +261,16 @@ public class Player : GameActor
     public void CancelSprint()
     {
         canSprint = false;
+        isAttack = false;
         rb.velocity = Vector2.zero;//强行终止冲刺
         Debug.Log("冲刺结束");
     }
+    /// <summary>
+    /// 也是冲刺结束时调用
+    /// </summary>
     public void SprintDone()
     {
-        sprintDone = true;
+        lastSprintDone = true;
     }
     
     #endregion
